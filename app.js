699d59392d6b0b092a314711ccddc9936829aa41
Javier <javier.cubides@avenida.com>
2016-04-04 16:43:20 -0300

    Added Angular and search

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ var multer = require('multer');
 var upload = multer({
   dest: 'uploads'
 });
+var io = require('socket.io')(3001);
 var csv = require('ya-csv');
 var iconv = require('iconv-lite');
 var https = require('http-debug').https
 https.debug = 0;
+var _ = require('underscore');
 
 
 var csvResponse = [];
+var csvLength = 0;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.render('index', {
-    title: 'Express'
+    title: 'Newsletter Web App'
   });
 });
 
@@ -81,23 +84,32 @@ app.post('/upload', upload.single('file'), function(req, res) {
             default:
               emailState.status = 'Ok';
           }
+          io.emit('csvParsing', emailState);
       });
       res.on('end', function () {
         csvResponse.push(emailState);
-        io.emit('csvResponse', csvResponse);
+        io.emit('csvProgress', {current: (csvResponse.length/csvLength)*100});
+        if (csvResponse.length == csvLength) {
+          io.emit('csvParsed', csvLength);
+          csvResponse = [];
+          csvLength = 0;
+        }
       })
     });
 
+    csvLength++;
     post_req.write(post_data);
     post_req.end();
   });
 
+  reader.addListener('end', function (data) {
+    console.log(csvLength);
+
+  })
+
   res.end('Se esta procesando el archivo');
 });
 
-io.on('connection', function(socket) {
-  socket.emit('csvResponse', csvResponse);
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
